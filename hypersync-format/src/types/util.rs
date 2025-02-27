424e61cd0ed4eabf6fe4028b72e1c1b86070e51f
Jono Prest <jjprest@gmail.com>
2025-02-27 19:04:13 +0200

    Fix last clippy warning

--- hypersync-format/src/types/util.rs
+++ hypersync-format/src/types/util.rs
@@ -1,5 +1,5 @@
 pub fn decode_hex(hex: &str) -> Result<Vec<u8>, faster_hex::Error> {
-    let len = hex.as_bytes().len();
+    let len = hex.len();
     let mut dst = vec![0; len / 2];
 
     faster_hex::hex_decode(hex.as_bytes(), &mut dst)?;
