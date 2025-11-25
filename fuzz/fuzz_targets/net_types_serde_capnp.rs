e45c8af2e2443bba692289f28ff11a6b6686f57d
Jono Prest <jjprest@gmail.com>
2025-11-25 10:09:56 +0200

    Fix query id edge case

--- fuzz/fuzz_targets/net_types_serde_capnp.rs
+++ fuzz/fuzz_targets/net_types_serde_capnp.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
 use hypersync_net_types::hypersync_net_types_capnp;
-use hypersync_net_types::{CapnpReader, Query};
+use hypersync_net_types::{request::QueryId, CapnpReader, Query};
 use libfuzzer_sys::fuzz_target;
 
 // Test if roundtripping the data through json produces same results
@@ -22,5 +22,23 @@ fuzz_target!(|data: Query| {
     assert_eq!(
         data, read_query,
         "capnp should read to the same value as the query"
+    );
+
+    let mut message = capnp::message::Builder::new_default();
+    let mut request_builder = message.init_root::<hypersync_net_types_capnp::request::Builder>();
+    request_builder.build_query_id_from_query(&data).unwrap();
+
+    let hypersync_net_types_capnp::request::body::Which::QueryId(Ok(bytes)) =
+        request_builder.into_reader().get_body().which().unwrap()
+    else {
+        panic!("should have read query id");
+    };
+
+    let query_id = QueryId::from_query(&data).unwrap();
+    let read_query_id = QueryId::from_query(&read_query).unwrap();
+    assert_eq!(query_id, read_query_id, "query id should match built from reconstructed query");
+    assert_eq!(
+        query_id.0.as_slice(), bytes,
+        "query id bytes in capnp body should match constructed query id"
     );
 });
