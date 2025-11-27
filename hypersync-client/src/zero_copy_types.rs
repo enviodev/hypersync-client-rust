4c0d13d0ce6eaa0d4b43409fc741dd954b584ad9
Jono Prest <jjprest@gmail.com>
2025-11-27 18:49:38 +0200

    Clean up api with intended behaviour

--- hypersync-client/src/zero_copy_types.rs
+++ hypersync-client/src/zero_copy_types.rs
@@ -3,21 +3,14 @@
 //! This module provides zero-copy readers that access Arrow columnar data directly
 //! without copying or allocating new memory for individual field access.
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use hypersync_format::{
     Address, BlockNumber, Data, FixedSizeData, Hash, LogIndex, TransactionIndex,
 };
+use hypersync_net_types::LogField;
 use polars_arrow::array::{BinaryArray, BooleanArray, StaticArray, UInt64Array};
 
 use crate::ArrowBatch;
-
-fn map_binary<'a, T>(i: usize, arr: Option<&'a BinaryArray<i32>>) -> Option<T>
-where
-    T: TryFrom<&'a [u8]>,
-    <T as TryFrom<&'a [u8]>>::Error: std::fmt::Debug,
-{
-    arr.and_then(|arr| arr.get(i).map(|v| T::try_from(v).unwrap()))
-}
 
 /// Zero-copy reader for log data from Arrow batches.
 ///
@@ -29,86 +22,129 @@ pub struct LogReader<'a> {
 }
 
 impl<'a> LogReader<'a> {
-    /// Create a new LogReader for a specific row in the batch
-    pub fn new(batch: &'a ArrowBatch, row_idx: usize) -> Self {
-        Self { batch, row_idx }
-    }
     /// The boolean value indicating if the event was removed from the blockchain due
     /// to a chain reorganization. True if the log was removed. False if it is a valid log.
     pub fn removed(&self) -> Result<Option<bool>> {
-        let array = self.batch.optional_column::<BooleanArray>("removed")?;
-        Ok(array.and_then(|arr| StaticArray::get(arr, self.row_idx)))
+        let array = self
+            .batch
+            .column::<BooleanArray>(LogField::Removed.as_ref())?;
+        Ok(array.get(self.row_idx))
     }
 
     /// The integer identifying the index of the event within the block's list of events.
-    pub fn log_index(&self) -> Result<Option<LogIndex>> {
-        let array = self.batch.optional_column::<UInt64Array>("log_index")?;
-        Ok(array.and_then(|arr| StaticArray::get(arr, self.row_idx).map(|v| v.into())))
+    pub fn log_index(&self) -> Result<LogIndex> {
+        let array = self
+            .batch
+            .column::<UInt64Array>(LogField::LogIndex.as_ref())?;
+        let value = array
+            .get(self.row_idx)
+            .context("value should not be null")?;
+        Ok(value.into())
     }
 
     /// The integer index of the transaction within the block's list of transactions.
-    pub fn transaction_index(&self) -> Result<Option<TransactionIndex>> {
+    pub fn transaction_index(&self) -> Result<TransactionIndex> {
         let array = self
             .batch
-            .optional_column::<UInt64Array>("transaction_index")?;
-        Ok(array.and_then(|arr| StaticArray::get(arr, self.row_idx).map(|v| v.into())))
+            .column::<UInt64Array>(LogField::TransactionIndex.as_ref())?;
+        let value = array
+            .get(self.row_idx)
+            .context("value should not be null")?;
+        Ok(value.into())
     }
 
     /// The hash of the transaction that triggered the event.
-    pub fn transaction_hash(&self) -> Result<Option<Hash>> {
+    pub fn transaction_hash(&self) -> Result<Hash> {
         let array = self
             .batch
-            .optional_column::<BinaryArray<i32>>("transaction_hash")?;
-        Ok(map_binary(self.row_idx, array))
+            .column::<BinaryArray<i32>>(LogField::TransactionHash.as_ref())?;
+        let value = array
+            .get(self.row_idx)
+            .context("value should not be null")?;
+        Hash::try_from(value).context("invalid hash format")
     }
 
     /// The hash of the block in which the event was included.
-    pub fn block_hash(&self) -> Result<Option<Hash>> {
+    pub fn block_hash(&self) -> Result<Hash> {
         let array = self
             .batch
-            .optional_column::<BinaryArray<i32>>("block_hash")?;
-        Ok(map_binary(self.row_idx, array))
+            .column::<BinaryArray<i32>>(LogField::BlockHash.as_ref())?;
+        let value = array
+            .get(self.row_idx)
+            .context("value should not be null")?;
+        Hash::try_from(value).context("Invalid hash format")
     }
 
     /// The block number in which the event was included.
-    pub fn block_number(&self) -> Result<Option<BlockNumber>> {
-        let array = self.batch.optional_column::<UInt64Array>("block_number")?;
-        Ok(array.and_then(|arr| StaticArray::get(arr, self.row_idx).map(|v| v.into())))
+    pub fn block_number(&self) -> Result<BlockNumber> {
+        let array = self
+            .batch
+            .column::<UInt64Array>(LogField::BlockNumber.as_ref())?;
+        let value = array
+            .get(self.row_idx)
+            .context("value should not be null")?;
+        Ok(value.into())
     }
 
     /// The contract address from which the event originated.
-    pub fn address(&self) -> Result<Option<Address>> {
-        let array = self.batch.optional_column::<BinaryArray<i32>>("address")?;
-        Ok(map_binary(self.row_idx, array))
+    pub fn address(&self) -> Result<Address> {
+        let array = self
+            .batch
+            .column::<BinaryArray<i32>>(LogField::Address.as_ref())?;
+        let value = array
+            .get(self.row_idx)
+            .context("value should not be null")?;
+        Address::try_from(value).context("Invalid address format")
     }
 
     /// The first topic of the event (topic0).
     pub fn topic0(&self) -> Result<Option<FixedSizeData<32>>> {
-        let array = self.batch.optional_column::<BinaryArray<i32>>("topic0")?;
-        Ok(map_binary(self.row_idx, array))
+        let array = self
+            .batch
+            .column::<BinaryArray<i32>>(LogField::Topic0.as_ref())?;
+        Ok(array
+            .get(self.row_idx)
+            .and_then(|v| FixedSizeData::<32>::try_from(v).ok()))
     }
 
     /// The second topic of the event (topic1).
     pub fn topic1(&self) -> Result<Option<FixedSizeData<32>>> {
-        let array = self.batch.optional_column::<BinaryArray<i32>>("topic1")?;
-        Ok(map_binary(self.row_idx, array))
+        let array = self
+            .batch
+            .column::<BinaryArray<i32>>(LogField::Topic1.as_ref())?;
+        Ok(array
+            .get(self.row_idx)
+            .and_then(|v| FixedSizeData::<32>::try_from(v).ok()))
     }
 
     /// The third topic of the event (topic2).
     pub fn topic2(&self) -> Result<Option<FixedSizeData<32>>> {
-        let array = self.batch.optional_column::<BinaryArray<i32>>("topic2")?;
-        Ok(map_binary(self.row_idx, array))
+        let array = self
+            .batch
+            .column::<BinaryArray<i32>>(LogField::Topic2.as_ref())?;
+        Ok(array
+            .get(self.row_idx)
+            .and_then(|v| FixedSizeData::<32>::try_from(v).ok()))
     }
 
     /// The fourth topic of the event (topic3).
     pub fn topic3(&self) -> Result<Option<FixedSizeData<32>>> {
-        let array = self.batch.optional_column::<BinaryArray<i32>>("topic3")?;
-        Ok(map_binary(self.row_idx, array))
+        let array = self
+            .batch
+            .column::<BinaryArray<i32>>(LogField::Topic3.as_ref())?;
+        Ok(array
+            .get(self.row_idx)
+            .and_then(|v| FixedSizeData::<32>::try_from(v).ok()))
     }
 
     /// The non-indexed data that was emitted along with the event.
-    pub fn data(&self) -> Result<Option<Data>> {
-        let array = self.batch.optional_column::<BinaryArray<i32>>("data")?;
-        Ok(map_binary(self.row_idx, array))
+    pub fn data(&self) -> Result<Data> {
+        let array = self
+            .batch
+            .column::<BinaryArray<i32>>(LogField::Data.as_ref())?;
+        let value = array
+            .get(self.row_idx)
+            .context("value should not be null")?;
+        Ok(Data::from(value))
     }
 }
